bef0aa5d5566ff545cb8c14289206a9c433fe9d3
ReesCodes <tenneyb0@gmail.com>
2024-01-25 02:23:54 -0500

    Add files via upload

--- badidea.py
+++ badidea.py
@@ -4,8 +4,8 @@ outFile = open("getEven.py","w")
 outFile.write("import sys\n")
 outFile.write("def getEven(a):\n" +
               "\t return")
-for i in range(90000):
+for i in range(10000):
     outFile.write(f" \"{'even' if(i%2==0) else 'odd'}\" if (a == {i}) else ")
 outFile.write('\"Unknown"\n')
-outFile.writelines(["if __name__ == \"__main__\":\n", "\tfor arg in sys.argv[1:]:\n", "\t\tprint(f\"{arg}: is {getEven(int(arg))})\n"])
+outFile.writelines(["if __name__ == \"__main__\":\n", "\tfor arg in sys.argv[1:]:\n", "\t\tprint(f\"{arg}: is {getEven(int(arg))}\")\n"])
 outFile.close()
