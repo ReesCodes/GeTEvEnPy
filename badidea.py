4e26f41a6593279e8ef1c27477c0abd15f8b8f10
ReesCodes <tenneyb0@gmail.com>
2024-01-25 02:20:26 -0500

    Add files via upload
    Here is a terrible idea I had to use the turnary operator to get if a number is even or odd
    Found out that the python parser has a max callstack limit but oh well

---
+++ badidea.py
@@ -0,0 +1,11 @@
+# This is a terible Idea
+
+outFile = open("getEven.py","w")
+outFile.write("import sys\n")
+outFile.write("def getEven(a):\n" +
+              "\t return")
+for i in range(90000):
+    outFile.write(f" \"{'even' if(i%2==0) else 'odd'}\" if (a == {i}) else ")
+outFile.write('\"Unknown"\n')
+outFile.writelines(["if __name__ == \"__main__\":\n", "\tfor arg in sys.argv[1:]:\n", "\t\tprint(f\"{arg}: is {getEven(int(arg))})\n"])
+outFile.close()
